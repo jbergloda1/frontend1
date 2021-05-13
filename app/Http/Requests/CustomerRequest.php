8a89484fb853607ffc992eee1a7bce6ce50f4882
jbergloda1 <jbloda1@gmail.com>
2021-05-13 17:46:15 +0700

    backend13/5

---
+++ app/Http/Requests/CustomerRequest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CustomerRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+    public function rules()
+    {
+        if($this->isMethod('post')){
+            return $this->storeRules();
+        }elseif($this->isMethod('put')){
+            return $this->updateRules();
+        }
+    }
+
+    //Store Customer
+    public function storeRules(): array
+    {
+        return [
+            'email'             => 'required|email|min:0|max:50|unique:customer',
+            'password'          => 'required|min:5|max:30',
+            'confirmpassword'   => 'required|same:password',
+        ];
+    }
+    public function storeFilter()
+    {
+        return $this->only([
+            'email',
+            'password'
+        ]);
+    }
+
+    //update Customer
+    public function updateRules(): array
+    {
+        return [
+            'email'             => 'required|email|unique:customer,email,'.$this->id,
+            'name'              => 'required|min:2|max:50',
+            'password'          => 'string|min:6|max:50|nullable',
+            'confirmpassword'   => 'string|min:6|max:50|same:password|nullable',
+            'phone'             => 'required|numeric',
+            'address'           => 'required|min:1|max:200'
+        ];
+    }
+    public function updateFilter()
+    {
+        return $this->only([
+            'email',
+            'name',
+            'password',
+            'phone',
+            'address'
+        ]);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,120 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| API Routes <===> E-commerce Website Shose Store
+|--------------------------------------------------------------------------
+|E-commerce Website Shose Store
+*/
+Route::group(['namespace' => 'Api', 'middleware' => ['cors']], function () {
+    /*
+    |--------------------------------------------------------------------------
+    | WEB PAGE
+    |--------------------------------------------------------------------------
+    */
+        //Show web page
+            // Sản phẩm theo nhà cung cấp
+            Route::get('product-supplier/{id}', 'HomeController@getProductSupplier')->name('product.supplier');
+            Route::get('product-category/{id}', 'HomeController@getProductCategory')->name('product.category');
+            //Color, Size theo Product
+            Route::get('product-color/{id}', 'HomeController@getProductColor')->name('product.color');
+            Route::get('product-size/{product}/{color}', 'HomeController@getProductSize')->name('product.size');
+            
+        //Product - Category - Supplier == WEB PAGE
+        Route::get('category', 'CategoryController@search');
+        Route::get('supplier', 'SupplierController@search');
+        Route::get('product', 'ProductController@search');
+        
+        //Login ADMIN
+            Route::post('login', 'LoginController@login')->name('login');
+
+        //Customer
+            Route::post('customer', 'CustomerController@store'); //Register customer
+            Route::put('customer/{id}', 'CustomerController@update');
+            Route::get('customer/{id}', 'CustomerController@show');
+            //Login WEB PAGE
+            Route::post('login-customer', 'CustomerController@login');
+
+        //Payment WEB PAGE
+        //Route::group(['middleware' => ['customer']], function () {
+            //Cart
+            Route::post('add-cart/{customer}', 'CartController@add');
+            Route::get('view-cart/{customer}', 'CartController@view');
+            Route::get('remove/{id}', 'CartController@remove');
+            Route::get('clear/{customer}', 'CartController@clear');
+            Route::put('update-cart/{id}', 'CartController@update');
+
+            //get total cart theo customer
+            Route::get('total-cart/{customer}', 'CartController@total');
+            
+            //Payment (store bill)
+            Route::post('bill/{customer}', 'BillController@store');
+        //});
+
+        //Logout
+        Route::get('logout', 'LoginController@logout');
+        // show product
+        Route::get('product/{id}', 'ProductController@show');
+        Route::get('product-new', 'ProductController@showNewest');
+    /*
+    |--------------------------------------------------------------------------
+    | ADMIN PAGE
+    |--------------------------------------------------------------------------
+    */
+    Route::group(['middleware' => ['auth']], function () {
+        Route::group(['middleware' => ['role']], function () {
+            /*
+            |--------------------------------------------------------------------------
+            | ADMIN - (role == 1)
+            |--------------------------------------------------------------------------
+            */
+                //Category
+                Route::post('category', 'CategoryController@store');
+                Route::get('category/{id}', 'CategoryController@show');
+                Route::put('category/{id}', 'CategoryController@update');
+
+                //Supplier
+                Route::post('supplier', 'SupplierController@store');
+                Route::get('supplier/{id}', 'SupplierController@show');
+                Route::put('supplier/{id}', 'SupplierController@update');
+
+                //Product
+
+                Route::post('product', 'ProductController@store');
+                Route::put('product/{id}', 'ProductController@update');
+                Route::delete('product/{id}', 'ProductController@destroy');
+
+                //User
+                Route::get('user', 'UserController@search');
+                Route::post('user', 'UserController@store');
+                Route::get('user/{id}', 'UserController@show');
+                Route::put('user/{id}', 'UserController@update');
+                Route::delete('user/{id}', 'UserController@destroy');
+
+                //Size Color
+                Route::get('size', 'Productcontroller@getsize');
+                Route::get('color', 'Productcontroller@getcolor');
+
+                //Token
+                Route::post('refresh', 'LoginController@refreshToken');
+                Route::delete('delete-token', 'LoginController@deleteToken');
+       });
+        /*
+            |--------------------------------------------------------------------------
+            | ADMIN - USER (role == 1 || role ==2)
+            |--------------------------------------------------------------------------
+        */
+                //User
+                Route::get('userprofile/{id}', 'UserController@showProfile');
+                Route::post('userprofile/{id}', 'UserController@updateProfile');
+
+                //Bill & BillDetail
+                Route::get('bill', 'BillController@search');
+                Route::get('bill/{id}', 'BillController@show')->name('bill.show');
+                Route::put('bill/{id}', 'BillController@update')->name('bill.update');
+                Route::delete('bill/{id}', 'BillController@destroy')->name('bill.destroy');
+                Route::get('statistical', 'BillController@statistical')->name('bill.statistical');
+
+                // //Logout
+                // Route::get('logout', 'LoginController@logout')->name('user.logout');
+   });
+});
